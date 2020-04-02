6dffd1d89199c7ef89cc17e91054758b274d51b5
radionch <63038294+radionch@users.noreply.github.com>
2020-04-02 13:09:33 +0600

    a program helps to identify the area of triangle

---
+++ areaoftriangle.rb
@@ -0,0 +1,6 @@
+puts "Please, enter the base of triangle "
+trianglebase = gets.chomp.to_f
+puts "Okay, now enter the height of triangle "
+triangleheight = gets.chomp.to_f
+areaoftriangle = (trianglebase * triangleheight) / 2
+puts "The area of triangle is :#{areaoftriangle} "
